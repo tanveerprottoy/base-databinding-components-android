5d95a3e7da8f693b5197bbb2f40fa0bb50588a0f
tanveerprottoy <tanveer.prottoy@gmail.com>
2018-11-23 22:01:43 +0600

    BaseObservable added

---
+++ basedatabindingcomponents/src/main/java/com/tanveershafeeprottoy/basedatabindingcomponents/observables/BaseObservable.kt
@@ -0,0 +1,34 @@
+package com.tanveershafeeprottoy.basedatabindingcomponents.observables
+
+import androidx.databinding.Observable
+import androidx.databinding.PropertyChangeRegistry
+
+open class BaseObservable : Observable {
+    private val propertyChangeRegistry: PropertyChangeRegistry = PropertyChangeRegistry()
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback) {
+        propertyChangeRegistry.add(callback)
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback) {
+        propertyChangeRegistry.remove(callback)
+    }
+
+    /**
+     * Notifies observers that all properties of this instance have changed.
+     */
+    fun notifyChange() {
+        propertyChangeRegistry.notifyCallbacks(this, 0, null)
+    }
+
+    /**
+     * Notifies observers that a specific property has changed. The getter for the
+     * property that changes should be marked with the @Bindable annotation to
+     * generate a field in the BR class to be used as the fieldId parameter.
+     *
+     * @param fieldId The generated BR id for the Bindable field.
+     */
+    fun notifyPropertyChanged(fieldId: Int) {
+        propertyChangeRegistry.notifyCallbacks(this, fieldId, null)
+    }
+}
