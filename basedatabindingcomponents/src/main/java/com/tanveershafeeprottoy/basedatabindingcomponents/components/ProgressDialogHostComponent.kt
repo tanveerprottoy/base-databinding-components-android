2feb8b2b86d85365fa02ea408af7f5c8e3d92183
tanveerprottoy <tanveer.prottoy@gmail.com>
2018-11-30 20:19:49 +0600

    Components added

---
+++ basedatabindingcomponents/src/main/java/com/tanveershafeeprottoy/basedatabindingcomponents/components/ProgressDialogHostComponent.kt
@@ -0,0 +1,11 @@
+package com.tanveershafeeprottoy.basedatabindingcomponents.components
+
+import androidx.databinding.ObservableBoolean
+
+/**
+ * Created by Tanveer Shafee Prottoy on 11/30/18.
+ */
+
+interface ProgressDialogHostComponent {
+    val showProgressBar: ObservableBoolean
+}
