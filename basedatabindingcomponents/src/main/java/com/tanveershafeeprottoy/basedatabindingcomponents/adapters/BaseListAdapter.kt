a1929bf9071914d9a18764c15e87a2943a3f0c5d
tanveerprottoy <tanveer.prottoy@gmail.com>
2018-12-17 23:21:19 +0600

    setData added Base list adapter

--- basedatabindingcomponents/src/main/java/com/tanveershafeeprottoy/basedatabindingcomponents/adapters/BaseListAdapter.kt
+++ basedatabindingcomponents/src/main/java/com/tanveershafeeprottoy/basedatabindingcomponents/adapters/BaseListAdapter.kt
@@ -7,7 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tanveershafeeprottoy.basedatabindingcomponents.listeners.ListItemOnClickListener
 import com.tanveershafeeprottoy.basedatabindingcomponents.viewholders.BaseRecyclerViewHolder
 
-open class BaseListAdapter(private val anyList: List<Any>?, private val resourceId: Int, private val variableId: Int, private val listItemOnClickListener: ListItemOnClickListener?) : RecyclerView.Adapter<BaseRecyclerViewHolder>() {
+open class BaseListAdapter(private val resourceId: Int, private val variableId: Int, private val listItemOnClickListener: ListItemOnClickListener?) : RecyclerView.Adapter<BaseRecyclerViewHolder>() {
+    private var anyList: List<Any>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseRecyclerViewHolder {
         return BaseRecyclerViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), resourceId, parent, false), variableId, listItemOnClickListener)
@@ -29,5 +30,9 @@ open class BaseListAdapter(private val anyList: List<Any>?, private val resource
         catch(n: NullPointerException) {
             0
         }
+    }
+
+    fun setData(anyList: List<Any>?) {
+        this.anyList = anyList
     }
 }
